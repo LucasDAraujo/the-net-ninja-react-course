3ce4f56ebef9d1df55e5fff9ebe6d3c3565c09d4
lucasDAraujo <lucasDAraujo>
2022-01-22 15:29:54 -0500

    Test click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
-    return <div className="home">
-        <h2>Homepage</h2>
-    </div>;
+    const handleClick = (ev) => {
+        console.log("Hello guys", ev);
+    };
+    const handleClickAgain = (name, ev) => {
+        console.log(`Hello ${name}, ${ev.target}`);
+    };
+    return (
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            {/* For calling another function with an argument, we need an anonymous function to wrap it  */}
+            <button onClick={(ev) => handleClickAgain("Mario", ev)}>
+                Click me again
+            </button>
+        </div>
+    );
 };
 
 export default Home;
