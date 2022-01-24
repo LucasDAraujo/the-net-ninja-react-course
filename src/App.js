e79a3e2a53953ce2e12218e34cfdcdcac0e6e881
lucasDAraujo <lucasDAraujo>
2022-01-24 13:50:07 -0500

    Add home route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 //ANCHOR  Imports
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 //ANCHOR Root component
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <div className="content">
-                <Home />
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                 <Switch>
+                     <Route path="/">
+                         <Home />
+                     </Route>
+                 </Switch>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
